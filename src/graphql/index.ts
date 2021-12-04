f82fcc160c68e773825c56ff0490f99b4cfb48bb
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-12-04 16:06:29 -0600

    created schema controller

---
+++ src/graphql/index.ts
@@ -0,0 +1,8 @@
+import { GraphQLSchema } from "graphql";
+import { makeExecutableSchema } from "graphql-tools";
+import "graphql-import-node";
+import rootSchema from "./schemas/schema.graphql";
+
+export const schema: GraphQLSchema = makeExecutableSchema({
+  typeDefs: [rootSchema],
+});
