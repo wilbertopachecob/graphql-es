7726f867c90cb59f2d81602821c83c688876d736
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2021-12-05 14:24:18 -0600

    added mutation resolver

--- src/graphql/resolvers/character.ts
+++ src/graphql/resolvers/character.ts
@@ -4,6 +4,16 @@ import data from "../../data/data.json";
 const characterResolver: IResolvers = {
   Query: {
     getCharacters: () => data.characters,
+    getCharacter: (parent: void, args: any) =>
+      data.characters.find((character) => character._id === args._id),
+  },
+  Mutation: {
+    addCharacter(parent: void, { character }: any) {
+      const _id = `${data.characters.length + 1}`;
+      data.characters.push({ _id, ...character });
+
+      return "The character was successfuly added";
+    },
   },
   Character: {
     games: (parent: any) =>
