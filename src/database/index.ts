ba92968552be75f24bba4a04aac41cd791b9be1d
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-12-06 16:37:05 -0600

    added getURI method

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,18 +1,29 @@
 import { MongoClient } from "mongodb";
 import { Db } from "mongodb";
+import config from "../config";
 
 let db: Db | undefined;
-const url = 'mongodb://localhost:27017';
+const getURI = () => {
+    let userInfo = '';
+    if (config.dbUsername && config.dbPassword) {
+        userInfo = `${config.dbUsername}:${config.dbPassword}@`
+    }    
+    let url = `mongodb://${userInfo}${config.dbHost}:${config.dbPort}`;
+    
+    return url;
+}
+
 // Database Name
-const dbName = 'graphql-es';
+const dbName = config.dbname;
 // Connect using MongoClient
-const mongoClient = new MongoClient(url);
+const mongoClient = new MongoClient(getURI());
 
 async function connect() {
     if (!db) {
         try {
             await mongoClient.connect();
             db = mongoClient.db(dbName);
+            console.log('MongoClient connection success');
             return db;
         } catch (error) {
             console.error(error);
