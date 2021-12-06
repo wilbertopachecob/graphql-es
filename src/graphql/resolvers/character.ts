bcb2a74b8dfd7ae1ec889b74764ad869a0659517
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-12-06 11:40:52 -0600

    refactored to use DB instead of local file

--- src/graphql/resolvers/character.ts
+++ src/graphql/resolvers/character.ts
@@ -1,18 +1,36 @@
 import { IResolvers } from "@graphql-tools/utils";
+import { ObjectId } from "mongodb";
+import { Db } from "mongodb";
 import data from "../../data/data.json";
 
 const characterResolver: IResolvers = {
   Query: {
-    getCharacters: () => data.characters,
-    getCharacter: (parent: void, args: any) =>
-      data.characters.find((character) => character._id === args._id),
+    async getCharacters(parent: void, args: any, context: Db) {
+      try {
+        const characters = await context.collection('characters').find().toArray(); 
+        return characters;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    getCharacter: async (parent: void, args: any, context: Db) => {
+      try {
+        const character = await context.collection('characters').findOne({"_id" : new ObjectId(args._id)})
+        return character;
+      } catch (error) {
+        console.log(error);
+      }
+    }
   },
   Mutation: {
-    addCharacter(parent: void, { character }: any) {
-      const _id = `${data.characters.length + 1}`;
-      data.characters.push({ _id, ...character });
-
-      return "The character was successfuly added";
+    async addCharacter(parent: void, { character }: any, context: Db) {
+      try {
+        await context.collection('characters').insertOne(character);  
+        return "The character was successfuly added";
+      } catch (error) {
+        console.log(error);
+        return 'Error inserting character in the Database' 
+      }
     },
   },
   Character: {
