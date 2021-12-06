7cd60b2b29994487205ec2edcb14ff8d969628db
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-12-06 11:41:18 -0600

    added DB context to Apollo

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
 import express from "express";
 import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
-import { schema } from './graphql/index';
+import { schema } from "./graphql/index";
+import GetDbInstance from "./database";
+import config from "./config";
 
 const app = express();
 const port = 5000;
 app.use(cors());
 
 async function startApolloServer() {
-    const server = new ApolloServer({
-        schema,
-        introspection: true
-    });
+  const server = new ApolloServer({
+    schema,
+    introspection: true,
+    context: await GetDbInstance(),
+  });
 
-    await server.start();
+  await server.start();
 
-    server.applyMiddleware({app})        ;
+  server.applyMiddleware({ app });
 }
 
 startApolloServer();
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+app.listen(config.port, () => {
+  console.log(`App listening at http://localhost:${config.port}`);
 });
-
