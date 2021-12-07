3c82f0ad7cdc957a2fcaf017542bdea14522a0ef
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2021-12-07 00:07:37 -0600

    added all resolvers for game

--- src/graphql/resolvers/game.ts
+++ src/graphql/resolvers/game.ts
@@ -1,9 +1,43 @@
 import { IResolvers } from "@graphql-tools/utils";
-import data from "../../data/data.json";
+import { Db, ObjectId } from "mongodb";
 
 const gameResolver: IResolvers = {
   Query: {
-    getGames: () => data.games,
+    getGames: async (parent: void, args: any, context: Db) => {
+      try {
+        return await context.collection("games").find().toArray();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  Mutation: {
+    addGame: async (parent: void, args: any, context: Db) => {
+      try {
+        await context.collection("games").insertOne(args.game);
+        return "Game created successfully";
+      } catch (error) {
+        console.log(error);
+        return "Error creating game";
+      }
+    },
+  },
+  Game: {
+    developers: async (parent: any, args: any, context: Db) => {
+      try {
+        const developers = await context
+          .collection("developers")
+          .find({
+            _id: {
+              $in: parent.developers.map((_id: string) => new ObjectId(_id)),
+            },
+          })
+          .toArray();
+        return developers;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 
