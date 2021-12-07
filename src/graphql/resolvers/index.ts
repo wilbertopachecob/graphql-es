fecaa77e5f74a078623981c45edc2d1c6cfc94b0
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-12-06 21:57:25 -0600

    added developer resolver to main resolvers

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,7 +1,8 @@
 import { mergeResolvers } from "@graphql-tools/merge";
 import characterResolver from "./character";
 import gameResolver from "./game";
+import developerResolver from "./developer";
 
-const resolvers = [characterResolver, gameResolver]
+const resolvers = [characterResolver, gameResolver, developerResolver];
 
 export default mergeResolvers(resolvers);
